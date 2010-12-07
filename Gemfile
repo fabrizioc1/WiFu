23e4734ed60bb6981e868cd9c3b988f3caa8fd12
fabrizioc1 <fabrizioc1@yahoo.com>
2010-12-07 08:27:10 -0500

    Copied Racket and Bit-Struct libraries from MSF3

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@
 source "http://rubygems.org"
 
 gem "pcaprub"
-gem "racket"
+
--- script/autowep.rb
+++ script/autowep.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 # If you have MetaSploit 3 installed you can run it using their included Ruby
 require 'rubygems'
-#require 'racket'
+#require "bundler/setup"
+puts "lib: #{File.expand_path('../../lib',__FILE__)}"
+$LOAD_PATH.unshift(File.expand_path('../../lib',__FILE__))
 require 'pcaprub'
+require 'racket'
 
 capture = Pcap.open_live("mon0",1344, true, 1)
 capture.each do |packet|
-  puts "#{packet}"
+  p = Racket::L2::Ethernet.new(packet)
+  puts "#{p.pretty}"
   puts
 end
 
