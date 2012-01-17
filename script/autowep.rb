f2fd1452c01a6a58011564ced6cb13f710a55a7e
Fabrizio Castrotorres <fabrizioc1@yahoo.com>
2012-01-16 23:12:43 -0500

    Saving autowep.rb before renaming and moving to examples

--- script/autowep.rb
+++ script/autowep.rb
@@ -11,15 +11,17 @@ TARGET_AP_ADDR = PacketFu::EthHeader.mac2str("68:7F:74:A3:C0:C5")
 BROADCAST_ADDR = PacketFu::EthHeader.mac2str("FF:FF:FF:FF:FF:FF")
 
 packet_count = 0
-capture = Pcap.open_live("mon0",0xffff, true, 1)
+#capture = Pcap.open_live("mon0",0xffff, true, 1)
+capture = Capture.new(:iface => "mon0", :start => true)
+
 puts "capture started ..."
 capture.each do |stream|
   packet = WifiPacket.new.read(stream)
   #if packet.is_beacon? && packet.src_addr == TARGET_AP_ADDR && packet.dst_addr == BROADCAST_ADDR
   if packet.is_beacon?
     puts packet
-    File.open("packet#{'%02d'%packet_count}.bin","wb"){ |file| file.write stream }
-    File.open("packet#{'%02d'%packet_count}.txt","w"){ |file| file.puts packet }
+    #File.open("packet#{'%02d'%packet_count}.bin","wb"){ |file| file.write stream }
+    #File.open("packet#{'%02d'%packet_count}.txt","w"){ |file| file.puts packet }
     packet_count+=1
   end
 end
